3e034fecf50114f3c377bada7e69409af9b924af
Srinath Kailasa <srinathkailasa@gmail.com>
2024-06-21 10:47:02 +0100

    CLN: Modern Setup using pyproject.toml (#41)
    * Modern setup using pyproject.toml

--- adaptoctree/__version.py
+++
@@ -1,3 +0,0 @@
-__title__ = 'adaptoctree'
-__version__ = '1.3.0'
-__description__ = 'Linear Adaptive Octrees in Python'
--- setup.py
+++ setup.py
@@ -1,36 +1,8 @@
-import os
-
 from setuptools import setup, find_packages
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-PATH_VERSION = os.path.join(HERE, 'adaptoctree', '__version.py')
-
-ABOUT = {}
-
-with open(PATH_VERSION, mode='r', encoding='utf-8') as f:
-    exec(f.read(), ABOUT)
-
-requirements = [
-    "numba==0.53.0",
-    "pytest==6.2.2"
-]
-
-
 setup(
-    name=ABOUT['__title__'],
-    version=ABOUT['__version__'],
-    description=ABOUT['__description__'],
-    license="BSD",
-    author="Srinath Kailasa",
-    author_email='srinathkailasa@gmail.com',
-    url='https://github.com/excalibur-sle/AdaptOctree',
     packages=find_packages(
         exclude=['*.test']
     ),
     zip_safe=False,
-    install_requires=requirements,
-    keywords='AdaptOctree',
-    classifiers=[
-        'Programming Language :: Python :: 3.8',
-    ]
 )
